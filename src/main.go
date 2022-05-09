6e8ec5792159311b3353f7513635077f5686d3ce
al3xdiaz <alexleonel96@hotmail.com>
2022-05-09 15:28:26 -0600

    feat: select to rutimes

--- src/main.go
+++ src/main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+func message(text string, c chan string){
+	c <- text
+}
+
 func main(){
-	fmt.Println("hola")
+	
+	sala1 := make(chan string)
+	sala2 := make(chan string)
+
+	go message("sala 1: bienvenidos...",sala1)
+	go message("sala 2: bienvenidas ...",sala2)
+
+	for i:=0; i < 2;i++{
+		select{
+		case m1 := <- sala1:
+			fmt.Println(m1)
+		case m2 := <- sala2:
+			fmt.Println(m2)
+		}
+	}
+	
 }
