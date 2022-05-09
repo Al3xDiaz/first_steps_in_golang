73d0309855d17c039789818e1a0b85f1f5131bc9
al3xdiaz <alexleonel96@hotmail.com>
2022-05-09 15:09:37 -0600

    coment

--- src/using_chanels.go
+++ src/using_chanels.go
@@ -6,6 +6,12 @@ func say(text string,c chan<- string){
 }
 
 func main(){
+	/*
+	create chanel
+	params:
+		1: type chanel
+		2: concurrent tasks numbers
+	*/
 	c:= make(chan string, 1)
 	fmt.Println("start process...")
 	go say("start go rutime with chanels",c)
